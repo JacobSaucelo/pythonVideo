ad724f035c02848cf6a24f8294aa034c7b5a8719
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-13 20:46:28 +0800

    working now

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ clip = clip.subclip(0, 15)
 cropClip = crop(clip, width=70, height=144, x_center=w/2, y_center=h/2)
 finalClip = cropClip.resize(width=100)
 
-text = TextClip("hewwo HEEEWWOO", fontsize=14, color="white", bg_color="blue")
-# .set_position("center").set_duration(10)
+text = TextClip("hewwo HEEEWWOO", fontsize=14, color="white", bg_color="blue").set_position("center").set_duration(10)
 
 final = CompositeVideoClip([finalClip, text])
 
-final.ipython_display(width = 70)  
-# final.write_videofile("clip.mp4")
+# final.ipython_display(width = 70)  
+final.write_videofile("clip.mp4")
