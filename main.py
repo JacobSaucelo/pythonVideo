0e7db6fba475224986ba245112c1ffa251320a0d
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-13 18:39:24 +0800

    now working resizables

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from moviepy.editor import VideoFileClip
+from moviepy.video.fx.all import crop
 
 clip = VideoFileClip("vidTemplate.mp4")
 
 clip = clip.subclip(0, 15)
+(w,h) = clip.size
 
-resizedVideo = clip.resize((100,144))
-resizedVideo.write_videofile("clip.mp4")
+
+cropClip = crop(clip, width=70, height=144, x_center=w/2, y_center=h/2)
+finalClip = cropClip.resize(width=100)
+
+finalClip.write_videofile("clip.mp4")
