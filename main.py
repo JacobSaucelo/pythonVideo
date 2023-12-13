0d905535015a8d3972cdf4addab24c28639da1c4
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-13 18:56:26 +0800

    backup installation prob

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from moviepy.editor import VideoFileClip
+from moviepy.editor import *
 from moviepy.video.fx.all import crop
 
 clip = VideoFileClip("vidTemplate.mp4")
@@ -10,4 +10,10 @@ clip = clip.subclip(0, 15)
 cropClip = crop(clip, width=70, height=144, x_center=w/2, y_center=h/2)
 finalClip = cropClip.resize(width=100)
 
-finalClip.write_videofile("clip.mp4")
+text = TextClip("hewwo HEEEWWOO", fontsize=14, color="white", bg_color="blue")
+# .set_position("center").set_duration(10)
+
+final = CompositeVideoClip([finalClip, text])
+
+final.ipython_display(width = 70)  
+# final.write_videofile("clip.mp4")
